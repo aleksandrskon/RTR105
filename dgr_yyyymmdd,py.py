4ac921b0e704a6f8f0bc8eaad3650bfda10e9f00
user <Aleksandrs.Konosonoks@inbox.lv>
2018-10-16 13:59:37 +0300

    yyyymmdd_hh_mm

---
+++ dgr_yyyymmdd,py.py
@@ -0,0 +1,228 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/user/RTR105/sc_1.py =====================
+5
+Blastoff!
+4
+4
+Blastoff!
+3
+3
+Blastoff!
+2
+2
+Blastoff!
+1
+1
+Blastoff!
+0
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+>>> n = 0
+>>> while n > 0 :
+    print("Lather")
+    print("Rince")
+    print("Dry off!")
+
+    
+>>> while n > 0 :
+	print("Lather")
+	print("Rince")print("Dry off!")
+	
+SyntaxError: invalid syntax
+>>> while n > 0 :
+	print("Lather")
+	print("Rince") print("Dry off!")
+	
+SyntaxError: invalid syntax
+>>> while n > 0 :
+	print("Lather")
+	print("Rince")
+	print("Dry off!")
+
+	
+>>> while n > 0 :
+	print("Lather")
+	print("Rince")
+print("Dry off!")
+SyntaxError: invalid syntax
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+>>> 
+===================== RESTART: /home/user/RTR105/sc_2.py =====================
+Dry off!
+>>> 
+===================== RESTART: /home/user/RTR105/sc_3.py =====================
+>hello there
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/sc_3.py", line 2, in <module>
+    line = input(">")
+  File "<string>", line 1
+    hello there
+              ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+===================== RESTART: /home/user/RTR105/sc_3.py =====================
+> hello there
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/sc_3.py", line 2, in <module>
+    line = input(">")
+  File "<string>", line 1
+    hello there
+              ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+===================== RESTART: /home/user/RTR105/sc_3.py =====================
+>hello there
+hello there
+>finished
+finished
+>done
+Done
+>>> 
+===================== RESTART: /home/user/RTR105/sc_3.py =====================
+>hello there
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/sc_3.py", line 2, in <module>
+    line = input(">")
+  File "<string>", line 1
+    hello there
+              ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+===================== RESTART: /home/user/RTR105/sc_3.py =====================
+>hello there
+hello there
+># don"t print this
+>print this!
+print this!
+>done
+Done
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+5
+4
+3
+2
+1
+Blastoff
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/sc_4.py", line 3, in <module>
+    print("Happy New Year:", friend)
+NameError: name 'friend' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/sc_4.py", line 3, in <module>
+    print("Happy New Year:", friend)
+NameError: name 'friend' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+Before
+9
+41
+12
+3
+74
+15
+After
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', -1)
+(9, 9)
+(41, 41)
+(41, 12)
+(41, 3)
+(74, 74)
+(74, 15)
+('After', 74)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', 0)
+(1, 9)
+(2, 41)
+(3, 12)
+(4, 3)
+(5, 74)
+(6, 15)
+('After', 6)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', 0)
+(9, 9)
+(50, 41)
+(62, 12)
+(65, 3)
+(139, 74)
+(154, 15)
+('After', 154)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', 0, 0)
+(1, 9, 9)
+(2, 50, 41)
+(3, 62, 12)
+(4, 65, 3)
+(5, 139, 74)
+(6, 154, 15)
+('After', 6, 154, 25)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+Before
+('Large number', 41)
+('Large number', 74)
+After
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', False)
+(False, 9)
+(False, 41)
+(False, 12)
+(True, 3)
+(True, 74)
+(True, 15)
+('After', True)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+('Before', -1)
+(-1, 9)
+(-1, 41)
+(-1, 12)
+(-1, 3)
+(-1, 74)
+(-1, 15)
+('After', -1)
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+>>> 
+===================== RESTART: /home/user/RTR105/sc_4.py =====================
+Before
+(3, 3)
+(3, 41)
+(3, 12)
+(3, 9)
+(3, 74)
+(3, 15)
+('After', 3)
+>>> 
---
+++ sc_3.py
@@ -0,0 +1,17 @@
+while True:
+    line = raw_input(">")
+    if line[0] == "#" :
+        continue
+    if line == "done" :
+        break
+    print(line)
+print("Done")
+
+"""
+while True:
+    line = raw_input(">")
+    if line == "done" :
+        break
+    print(line)
+print("Done")
+"""
